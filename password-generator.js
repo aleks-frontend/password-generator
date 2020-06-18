a62973d141c2361ac1c81ec44ae1b8e8eb9b826c
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2020-06-18 11:29:07 +0200

    feat: copy to clipboard

--- password-generator.js
+++ password-generator.js
@@ -1,4 +1,4 @@
-$(document).on('knack-view-render.view_1', function(event, view, data) {
+$(document).on('knack-view-render.view_1', function (event, view, data) {
     var $self = $(this);
   
     // Setting some global variables (IDs of these will need to be changed depending on the View)
@@ -6,51 +6,68 @@ $(document).on('knack-view-render.view_1', function(event, view, data) {
     var $passwordLength = $self.find('#view_1-field_5');
     var $passwordInputContainer = $self.find('#kn-input-field_3');
     var $passwordInput = $self.find('#field_3');
-    
+  
     // Helper function for generating a random string
     function generateRandom(charset, length) {
-      var retVal = "";    
+      var retVal = "";
       for (var i = 0, n = charset.length; i < length; ++i) {
-          retVal += charset.charAt(Math.floor(Math.random() * n));
+        retVal += charset.charAt(Math.floor(Math.random() * n));
       }
-      
+  
       return retVal;
     }
-    
+  
     // Helper function for shuffling the characters
     function shuffleString(s) {
-        var arr = s.split('');
-    
-        return arr.sort(function() {
-          return 0.5 - Math.random();
-        }).join('');	
+      var arr = s.split('');
+  
+      return arr.sort(function () {
+        return 0.5 - Math.random();
+      }).join('');
+    }
+  
+    // Helper function for setting the password password input field
+    function setPasswordToInput() {
+      $passwordInput.val(getNewPassword());
     }
     
+    // Helper function for copying the password to clipboard
+    function copyPasswordToClipboard() {
+      $passwordInput.select();
+      document.execCommand('copy');
+    }
+    
+    // Helper function for setting and copying the password
+    function setAndCopyPassword() {
+      setPasswordToInput();
+      copyPasswordToClipboard()
+    }
+  
     // Main function for generating the password
-    function getNewPassword() {    
+    function getNewPassword() {
       // Getting the password length from dropdown
       var length = $passwordLength.val();
-      
+  
       // Getting the password complexity options
       var includeUppercase = $passwordComplexity.find('input[value="Include Uppercase"]').is(':checked');
       var includeNumbers = $passwordComplexity.find('input[value="Include Numbers"]').is(':checked');
       var includeSymbols = $passwordComplexity.find('input[value="Include Symbols"]').is(':checked');
       var excludeAmbigousChars = $passwordComplexity.find('input[value="Exclude Ambiguous Characters"]').is(':checked');
-      
-      
+  
+  
       // Characters that can be used when trying to avoid chars that could be easily confused: !#%+23456789:=?@ABCDEFGHJKLMNPRSTUVWXYZabcdefghijkmnopqrstuvwxyz
       // we use these when 'Exclude Ambiguous Characters' checkbox is checked
       var simplePasswLowercase = 'abcdefghijkmnopqrstuvwxyz';
       var simplePasswUppercase = 'ABCDEFGHJKLMNPRSTUVWXYZ';
       var simplePasswNumbers = '23456789';
       var simplePasswSymbols = '!#%+:=?@';
-              
+  
       // All symbols that can be used in passwords
       var complexPasswSymbols = '!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~';
-      
+  
       // Alphabet characters
       var alphabet = 'abcdefghijklmnopqrstuvwxyz';
-      
+  
       if (!includeUppercase && !includeNumbers && !includeSymbols && !excludeAmbigousChars) {
         return generateRandom(alphabet, length);
       } else {
@@ -67,25 +84,25 @@ $(document).on('knack-view-render.view_1', function(event, view, data) {
           case 3:
             percentageToBeUsed = 20;
             break;
-          default: 
+          default:
             percentageToBeUsed = 0;
-        }     
-        
-        var calculatedPercentage = Math.round(length/100 * percentageToBeUsed);
-        
-        var uppercaseNeeded = includeUppercase ? calculatedPercentage : 0;            
-        var numbersNeeded = includeNumbers ? calculatedPercentage : 0;            
-        var symbolsNeeded = includeSymbols ? calculatedPercentage : 0;      
-        
+                                    }
+  
+        var calculatedPercentage = Math.round(length / 100 * percentageToBeUsed);
+  
+        var uppercaseNeeded = includeUppercase ? calculatedPercentage : 0;
+        var numbersNeeded = includeNumbers ? calculatedPercentage : 0;
+        var symbolsNeeded = includeSymbols ? calculatedPercentage : 0;
+  
         var normalCharsNeeded = length - uppercaseNeeded - numbersNeeded - symbolsNeeded;
-        
+  
         var normalChars = '',
             uppercaseChars = '',
             numberChars = '',
             symbolChars = '';
-        
+  
         normalChars = generateRandom(excludeAmbigousChars ? simplePasswLowercase : alphabet, normalCharsNeeded);
-          
+  
         if (uppercaseNeeded > 0) {
           uppercaseChars = generateRandom(excludeAmbigousChars ? simplePasswUppercase : alphabet.toUpperCase(), uppercaseNeeded);
         }
@@ -96,23 +113,24 @@ $(document).on('knack-view-render.view_1', function(event, view, data) {
   
         if (symbolsNeeded > 0) {
           symbolChars = generateRandom(excludeAmbigousChars ? simplePasswSymbols : complexPasswSymbols, symbolsNeeded);
-        } 
-        
+        }
+  
         return shuffleString(normalChars + uppercaseChars + numberChars + symbolChars);
-        
-      }    
+  
+      }
     }
-    
-    
+  
+  
     // Adding Regenerate button to HTML structure
     $passwordInputContainer.append('<button id="password-regenerate-btn">Regenerate</button>');
-    
+  
     // Event Handlers
-    $('#password-regenerate-btn').on('click', function(e) {
-      $passwordInput.val(getNewPassword());
+    $('#password-regenerate-btn').on('click', function (e) {
+      setAndCopyPassword();
       e.preventDefault();
     });
   
-    $passwordComplexity.on('change', function(e) { $passwordInput.val(getNewPassword()) });
-    $passwordLength.on('change', function(e) { $passwordInput.val(getNewPassword()) });  
+    $passwordComplexity.on('change', function (e) { setAndCopyPassword() });
+    $passwordLength.on('change', function (e) { setAndCopyPassword() });
   });
+  
